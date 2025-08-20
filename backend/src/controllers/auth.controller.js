aa5d2cf39092d2b13b4e7fed4d9c8c8b9baafc9f
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-08-20 18:52:19 +0600

    jwt scaffolding

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,30 @@
-export const signup = (req, res) => {
-  res.send("Signup routes");
+import User from "../models/user.model";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be atleast 6 characters" });
+    }
+    const user = User;
+    if (user) res.status(400).json({ message: "Email already exists" });
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      //generate jwt token
+    } else {
+      res.status(400).json({ message: "Invalid user Data" });
+    }
+  } catch (err) {}
 };
 
 export const login = (req, res) => {
---
+++ backend/src/lib/utils.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (user) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+  res.cookie("token", token, {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV !== "development",
+  });
+  return token;
+};
