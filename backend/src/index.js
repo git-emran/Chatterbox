5f48e451aba219a029562d821953075a1510b837
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-08-25 14:24:34 +0600

    jwt implementation

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,13 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());
 const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -3,6 +3,27 @@ import User from "../models/user.model.js";
 
 export const protectRoute = async (req, res, next) => {
   try {
-    const token = res.cookies.jwt;
-  } catch (error) {}
+    const token = req.cookies.jwt;
+
+    if (!token) {
+      res.status(401).json({ message: "Unauthorized - No token provided" });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded) {
+      res.status(401).json({ message: "Invalid token" });
+    }
+
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      res.status(401).json({ message: "User not found" });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.log("Error in protectRoute middleware", error.message);
+  }
 };
